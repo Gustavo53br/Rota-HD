3bef3b3f0c9f7b0585411ddd6c61463d2392b5d6
Gustavo Brisola <73241359+Gustavo53br@users.noreply.github.com>
2021-10-14 22:26:04 -0300

    newcomit

--- script.js
+++ script.js
@@ -20,15 +20,4 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 
 //*slide*//
 
-function typeWriter(elemento) {
-  const textoArray = elemento.innerHTML.split('');
-  elemento.innerHTML = '';
-  textoArray.forEach((letra, i) => {
-    setTimeout(() => elemento.innerHTML += letra, 75 * i);
-  });
-}
-
-
-const titulo = document.querySelector('h2');
-typeWriter(titulo);
 
